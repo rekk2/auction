0abe0f30e1ae450fcad884929759a0eff254d45e
rekk2 <32183761+rekk2@users.noreply.github.com>
2023-06-03 17:37:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,23 +9,27 @@ echo "Please enter the title for the project:"
 read TITLE
 
 # Set Timezone
-sudo timedatectl set-timezone America/New_York || exit 1
+sudo timedatectl set-timezone America/New_York
+
+# Updating system
+sudo apt-get update
+sudo apt-get upgrade -y
 
 # Installing necessary packages
-sudo apt-get install -y python3-venv python3-pip git nginx || exit 1
+sudo apt-get install -y python3-venv python3-pip git nginx
 
 # The path to your HTML file
 HTML_FILE="${PROJECT_PATH}/templates/auction.html"
 
 # Insert the title using sed
-sed -i "s|<title>.*</title>|<title>$TITLE</title>|" "$HTML_FILE" || exit 1
+sed -i "s|<title>.*</title>|<title>$TITLE</title>|" "$HTML_FILE"
 
 # Creating a virtual environment
-python3 -m venv venv || exit 1
-source venv/bin/activate || exit 1
+python3 -m venv venv
+source venv/bin/activate
 
 # Installing Python packages
-pip install -r requirements.txt || exit 1
+pip install -r requirements.txt
 
 # Creating a systemd service file
 echo "[Unit]
@@ -39,20 +43,22 @@ WorkingDirectory=$PROJECT_PATH
 ExecStart=${PROJECT_PATH}/venv/bin/gunicorn -b :8080 auction:app
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/myproject.service || exit 1
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/myproject.service
 
 # Starting the service
-sudo systemctl daemon-reload || exit 1
-sudo systemctl start myproject || exit 1
-sudo systemctl enable myproject || exit 1
+sudo systemctl daemon-reload
+sudo systemctl start myproject
+sudo systemctl enable myproject
 
 # Checking the status of the service
-sudo systemctl status myproject || exit 1
+sudo systemctl status myproject
 
 # Configuring nginx
-sudo systemctl start nginx || exit 1
+sudo systemctl start nginx
 
-echo "server {
+# Write Nginx configuration for the project
+sudo bash -c "cat > /etc/nginx/sites-available/myproject" << EOF
+server {
     listen 80;
     server_name _;
 
@@ -63,12 +69,16 @@ echo "server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
-}" | sudo tee /etc/nginx/sites-available/myproject || exit 1
+}
+EOF
 
-sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled/ || exit 1
+# Creating a symbolic link
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled/
 
-# Remove default Nginx configuration
-sudo rm -f /etc/nginx/sites-enabled/default || exit 1
+# Remove default Nginx configuration if it exists
+if [ -e /etc/nginx/sites-enabled/default ]; then
+    sudo rm /etc/nginx/sites-enabled/default
+fi
 
-sudo nginx -t || exit 1
-sudo systemctl reload nginx || exit 1
+# Test Nginx configuration and reload if it's okay
+sudo nginx -t && sudo systemctl reload nginx

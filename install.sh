98945752d83fa2993696d64281a2c2955e86b2b1
rekk2 <32183761+rekk2@users.noreply.github.com>
2023-06-03 10:43:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Setting the username and project path and title
-USERNAME="your_username"
-PROJECT_PATH="/auction"
-TITLE="Your Title"
+USERNAME=$(whoami)
+PROJECT_PATH=$(pwd)
+
+# Prompt the user for the title
+echo "Please enter the title for the project:"
+read TITLE
 
 # Set Timezone
 sudo timedatectl set-timezone America/New_York
@@ -16,18 +19,18 @@ sudo apt-get upgrade -y
 sudo apt-get install -y python3-venv python3-pip git nginx
 
 # The path to your HTML file
-HTML_FILE="/templates/auction.html"
+HTML_FILE="${PROJECT_PATH}/templates/auction.html"
 
 # Insert the title using sed
-sed -i "6s|<title>|<title>$TITLE|" "$HTML_FILE"
+sed -i "s|<title>.*</title>|<title>$TITLE</title>|" "$HTML_FILE"
 
 # Creating a virtual environment
-sudo python3 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 # Installing Python packages
-sudo pip install flask
-sudo pip install gunicorn
+pip install flask
+pip install gunicorn
 
 # Creating a systemd service file
 echo "[Unit]
@@ -39,13 +42,10 @@ User=$USERNAME
 Group=www-data
 WorkingDirectory=$PROJECT_PATH
 Environment=\"PATH=$PROJECT_PATH/venv/bin\"
-ExecStart=/usr/local/bin/gunicorn -b :8080 auction:app
+ExecStart=${PROJECT_PATH}/venv/bin/gunicorn -b :8080 auction:app
 
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/myproject.service
-
-# Symlinking the project to the venv
-ln -s $PROJECT_PATH $PROJECT_PATH/venv/bin
 
 # Starting the service
 sudo systemctl daemon-reload
